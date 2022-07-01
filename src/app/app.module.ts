fcbc86706ec8a1b9969ecea28eed313821d1c666
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-07-01 16:47:33 -0300

    Implementando pipe para corrigir caminho de imagem, usando o mesmo em um componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AuthGuard } from './services/app.guard';
 import { CadastroGuard } from './services/cadastro.guard';
 import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
 import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
+import { ImageFormaterPipe } from './demos/pipes/filmes/image.pipe';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
     SobreComponent,
     CadastroComponent,
     FilmesComponent,
-    FileSizePipe    
+    FileSizePipe,
+    ImageFormaterPipe
   ],
   imports: [
     BrowserModule,
--- src/app/demos/pipes/filmes/filmes.component.ts
+++ src/app/demos/pipes/filmes/filmes.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Filme } from './filme';
+import { ImageFormaterPipe } from './image.pipe';
 
 @Component({
   selector: 'app-filmes',
-  templateUrl: './filmes.component.html'  
+  templateUrl: './filmes.component.html',
+  providers: [
+    ImageFormaterPipe
+  ]  
 })
 export class FilmesComponent implements OnInit {
 
-  filmes: Filme[];   
+  filmes: Filme[];
+  mapped: Filme[];
+
+  constructor(private imageFormat: ImageFormaterPipe){}
 
   ngOnInit() {
 
@@ -44,11 +51,20 @@ export class FilmesComponent implements OnInit {
         nome: 'Pulp Fiction: Tempo de Violência ',
         dataLancamento: new Date('01/08/1994'),
         valor: 190.00,
-        imagem: 'PulpFiction.jpg',
+        imagem: '',
         tamanho: '773039680'
       }
     ];
 
+    this.mapped = this.filmes.map(filme => {
+      return {
+        nome: filme.nome,
+        dataLancamento: filme.dataLancamento,
+        valor: filme.valor,
+        tamanho: filme.tamanho,
+        imagem: this.imageFormat.transform(filme.imagem, 'default', false)
+      }
+    });
     
   }
 }
